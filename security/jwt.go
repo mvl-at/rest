74eb4c05267ec2f5272a90ea7f28ae2a8e6ee625
Richard Stöckl <richard.stoeckl@aon.at>
2018-06-02 14:51:35 +0200

    separated hash

--- security/jwt.go
+++ security/jwt.go
@@ -59,10 +59,14 @@ func generateToken(member *model.Member) string {
 	encodedHead := base64.URLEncoding.EncodeToString([]byte(head))
 	encodedPayload := base64.URLEncoding.EncodeToString([]byte(pay))
 	rawToken := encodedHead + "." + encodedPayload
+	fullToken := rawToken + "." + hash(rawToken)
+	return fullToken
+}
+
+func hash(rawToken string) string {
 	sig := hmac.New(sha256.New, []byte(context.Conf.JwtSecret))
 	sig.Write([]byte(rawToken))
-	fullToken := rawToken + "." + hex.EncodeToString(sig.Sum(nil))
-	return fullToken
+	return hex.EncodeToString(sig.Sum(nil))
 }
 
 func fetchMember(id int64) *model.Member {
@@ -73,14 +77,21 @@ func fetchMember(id int64) *model.Member {
 
 func Check(token string) (valid bool, member *model.Member) {
 	tokenParts := strings.Split(token, ".")
+	valid = false
+
+	if hash(tokenParts[0]+"."+tokenParts[1]) != tokenParts[2] {
+		return
+	}
+
 	payloadJsonByte, _ := base64.URLEncoding.DecodeString(tokenParts[1])
 	payload := &JWTPayload{}
 	json.Unmarshal(payloadJsonByte, payload)
+
+	if time.Now().After(payload.Expiration) {
+		return
+	}
+
 	member = fetchMember(payload.MemberId)
 	valid = true
-	if token != generateToken(member) || time.Now().After(payload.Expiration) {
-		member = nil
-		valid = false
-	}
 	return
 }
