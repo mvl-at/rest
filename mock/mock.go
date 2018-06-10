045d41753a9e07338e66d8ca24e74b0a7d2d1a35
Richard Stöckl <richard.stoeckl@aon.at>
2018-06-10 19:13:15 +0200

    export mock data

--- mock/mock.go
+++ mock/mock.go
@@ -6,130 +6,105 @@ import (
 	"time"
 )
 
+var Fruehschoppen = &model.Event{
+	Name:          "Frühschoppen",
+	Date:          time.Date(2018, 6, 17, 0, 0, 0, 0, time.Local),
+	Uniform:       "MVL Polo und Lederhose",
+	MusicianPlace: "Musikheim",
+	Place:         "Musikheim",
+	Time:          time.Date(1, 1, 1, 10, 0, 0, 0, time.Local),
+	MusicianTime:  time.Date(0, 0, 0, 8, 45, 0, 0, time.Local),
+	Note:          "Anschl. Wegräumen",
+	Important:     true,
+	Internal:      false}
+
+var Marschmusikwertung = &model.Event{
+	Name:          "Marschmusikwertung",
+	Date:          time.Date(2018, 9, 17, 0, 0, 0, 0, time.Local),
+	Uniform:       "Uniform mit Hut",
+	MusicianPlace: "Musikheim",
+	Place:         "Spannberg",
+	Time:          time.Date(1, 1, 1, 13, 0, 0, 0, time.Local),
+	MusicianTime:  time.Date(0, 0, 0, 11, 0, 0, 0, time.Local),
+	Important:     false,
+	Internal:      false}
+
+var Generalversammlung = &model.Event{
+	Name:          "Generalversammlung",
+	Date:          time.Date(2018, 3, 17, 0, 0, 0, 0, time.Local),
+	MusicianPlace: "Musikheim",
+	MusicianTime:  time.Date(0, 0, 0, 20, 0, 0, 0, time.Local),
+	Important:     true,
+	Internal:      true}
+
+var Fluegelhorn = &model.Instrument{NamePlural: "Flügelhörner", Name: "Flügelhorn"}
+var Horn = &model.Instrument{Name: "Waldhorn", NamePlural: "Waldhörner"}
+var Tenorhorn = &model.Instrument{NamePlural: "Tenorhörner", Name: "Tenorhorn"}
+
+var Paul = &model.Member{
+	FirstName:    "Paul",
+	LastName:     "Blatt",
+	Joined:       1993,
+	Active:       true,
+	Deleted:      false,
+	LoginAllowed: true,
+	Instrument:   Fluegelhorn,
+	Username:     "willi",
+	Password:     "123456"}
+
+var Helmut = &model.Member{
+	FirstName:    "Helmut",
+	LastName:     "Gras",
+	Joined:       1975,
+	Active:       true,
+	Deleted:      false,
+	LoginAllowed: true,
+	Instrument:   Tenorhorn}
+var Karl = &model.Member{
+	FirstName:    "Karl",
+	LastName:     "Baum",
+	Joined:       2014,
+	Active:       true,
+	Deleted:      false,
+	LoginAllowed: true,
+	Instrument:   Fluegelhorn}
+var Franz = &model.Member{
+	FirstName:    "Franz",
+	LastName:     "Moos",
+	Joined:       2011,
+	Active:       false,
+	Deleted:      true,
+	LoginAllowed: false,
+	Instrument:   Horn}
+var Josef = &model.Member{
+	FirstName:    "Josef",
+	LastName:     "Strauch",
+	Joined:       2009,
+	Active:       true,
+	Deleted:      false,
+	LoginAllowed: true,
+	Instrument:   Tenorhorn}
+
+var Obmann = &model.LeaderRole{Name: "Obmann", NamePlural: "Obmänner"}
+var Archivar = &model.LeaderRole{Name: "Archivar", NamePlural: "Archivare"}
+var Root = &model.Role{Id: "root", Name: "Root", NamePlural: "Root"}
+var Instrumente = &model.Role{Id: "instrument", Name: "Instrumenten Manager", NamePlural: "Instrumenten Manager"}
+var PaulObmann = &model.LeaderRoleMember{LeaderRole: Obmann, Member: Paul, Priority: 0}
+var HelmutArchivar = &model.LeaderRoleMember{LeaderRole: Archivar, Member: Helmut, Priority: 0}
+var PaulArchivarStellvertreter = &model.LeaderRoleMember{LeaderRole: Archivar, Member: Paul, Priority: 1}
+var JosefRoot = &model.RoleMember{Role: Root, Member: Josef}
+var PaulInstrumente = &model.RoleMember{Role: Instrumente, Member: Paul}
+var Events = &model.Role{Id: "event", Name: "Termine", NamePlural: "Termine"}
+var HelmutEvents = &model.RoleMember{Member: Helmut, Role: Events}
+
 func MockData() {
-	events := []*model.Event{
-		{
-			Name:          "Frühschoppen",
-			Date:          time.Date(2018, 6, 17, 0, 0, 0, 0, time.Local),
-			Uniform:       "MVL Polo und Lederhose",
-			MusicianPlace: "Musikheim",
-			Place:         "Musikheim",
-			Time:          time.Date(1, 1, 1, 10, 0, 0, 0, time.Local),
-			MusicianTime:  time.Date(0, 0, 0, 8, 45, 0, 0, time.Local),
-			Note:          "Anschl. Wegräumen",
-			Important:     true,
-			Internal:      false},
-		{
-			Name:          "Marschmusikwertung",
-			Date:          time.Date(2018, 9, 17, 0, 0, 0, 0, time.Local),
-			Uniform:       "Uniform mit Hut",
-			MusicianPlace: "Musikheim",
-			Place:         "Spannberg",
-			Time:          time.Date(1, 1, 1, 13, 0, 0, 0, time.Local),
-			MusicianTime:  time.Date(0, 0, 0, 11, 0, 0, 0, time.Local),
-			Important:     false,
-			Internal:      false},
-		{
-			Name:          "Generalversammlung",
-			Date:          time.Date(2018, 3, 17, 0, 0, 0, 0, time.Local),
-			MusicianPlace: "Musikheim",
-			MusicianTime:  time.Date(0, 0, 0, 20, 0, 0, 0, time.Local),
-			Important:     true,
-			Internal:      true}}
-
-	flg := &model.Instrument{
-		NamePlural: "Flügelhörner",
-		Name:       "Flügelhorn"}
-	hrn := &model.Instrument{
-		Name:       "Waldhorn",
-		NamePlural: "Waldhörner"}
-	ten := &model.Instrument{NamePlural: "Tenorhörner", Name: "Tenorhorn"}
-
-	willi := &model.Member{
-		FirstName:    "Willi",
-		LastName:     "Herok",
-		Joined:       1993,
-		Active:       true,
-		Deleted:      false,
-		LoginAllowed: true,
-		Instrument:   flg,
-		Username:     "willi",
-		Password:     "123456"}
-
-	felix := &model.Member{
-		FirstName:    "Felix",
-		LastName:     "Nentwich",
-		Joined:       1975,
-		Active:       true,
-		Deleted:      false,
-		LoginAllowed: true,
-		Instrument:   ten}
-
-	david := &model.Member{
-		FirstName:    "David",
-		LastName:     "Hörler",
-		Joined:       2014,
-		Active:       true,
-		Deleted:      false,
-		LoginAllowed: true,
-		Instrument:   flg}
-
-	leo := &model.Member{
-		FirstName:    "Leonard",
-		LastName:     "Kovarik",
-		Joined:       2011,
-		Active:       false,
-		Deleted:      true,
-		LoginAllowed: false,
-		Instrument:   hrn}
-
-	obm := &model.LeaderRole{
-		Name:       "Obmann",
-		NamePlural: "Obmänner"}
-
-	arc := &model.LeaderRole{
-		Name:       "Archivar",
-		NamePlural: "Archivare"}
-
-	alk := &model.Role{
-		Id:         "alko",
-		Name:       "Alkoholrat",
-		NamePlural: "Alkoholräte"}
-
-	ins := &model.Role{
-		Id:         "instrument",
-		Name:       "Instrumenten Manager",
-		NamePlural: "Instrumenten Manager"}
-
-	willob := &model.LeaderRoleMember{
-		LeaderRole: obm,
-		Member:     willi,
-		Priority:   0}
-
-	felarc := &model.LeaderRoleMember{
-		LeaderRole: arc,
-		Member:     felix,
-		Priority:   0}
-
-	willarc := &model.LeaderRoleMember{
-		LeaderRole: arc,
-		Member:     willi,
-		Priority:   1}
-
-	felalk := &model.RoleMember{
-		Role:   alk,
-		Member: felix}
-
-	willins := &model.RoleMember{
-		Role:   ins,
-		Member: willi}
-
-	members := []*model.Member{willi, felix, david, leo}
-	instruments := []*model.Instrument{flg, hrn, ten}
-	roles := []*model.Role{alk, ins}
-	leaderRoles := []*model.LeaderRole{obm, arc}
-	roleMembers := []*model.RoleMember{felalk, willins}
-	leaderRoleMembers := []*model.LeaderRoleMember{willarc, willob, felarc}
+	events := []*model.Event{Fruehschoppen, Generalversammlung, Marschmusikwertung}
+	members := []*model.Member{Paul, Helmut, Karl, Franz, Josef}
+	instruments := []*model.Instrument{Fluegelhorn, Horn, Tenorhorn}
+	roles := []*model.Role{Root, Instrumente}
+	leaderRoles := []*model.LeaderRole{Obmann, Archivar}
+	roleMembers := []*model.RoleMember{JosefRoot, PaulInstrumente, HelmutEvents}
+	leaderRoleMembers := []*model.LeaderRoleMember{PaulArchivarStellvertreter, PaulObmann, HelmutArchivar}
 
 	for _, v := range events {
 		database.Save(v)
--- security_test/security_test.go
+++ security_test/security_test.go
@@ -2,11 +2,12 @@ package security_test
 
 import (
 	_ "github.com/mattn/go-sqlite3"
+	. "rest/mock"
 	"rest/model"
 	"testing"
 )
 
 func TestPaulInsertTuba(t *testing.T) {
 	tuba := &model.Instrument{Name: "Tuba", NamePlural: "Tuben"}
-	saveData(tuba, true, willi, t)
+	saveData(tuba, true, Paul, t)
 }
