15ae9d7d5f7f5b553b2a5c6b6352076e64e8b731
Richard Stöckl <richard.stoeckl@aon.at>
2018-06-10 20:00:43 +0200

    added security tests

--- security_test/security_test.go
+++ security_test/security_test.go
@@ -5,6 +5,7 @@ import (
 	. "rest/mock"
 	"rest/model"
 	"testing"
+	"time"
 )
 
 func TestPaulInsertTuba(t *testing.T) {
@@ -15,4 +16,47 @@ func TestPaulInsertTuba(t *testing.T) {
 func TestJosefInsertTrombone(t *testing.T) {
 	trombone := &model.Instrument{Name: "Posaune", NamePlural: "Posaunen"}
 	saveData(trombone, true, Josef, t)
+}
+
+func TestHelmutInsertTrumpet(t *testing.T) {
+	trumpet := &model.Instrument{Name: "Trompete", NamePlural: "Trompete"}
+	saveData(trumpet, false, Helmut, t)
+}
+
+func TestJosefInsertKrampuskonzert(t *testing.T) {
+	event := &model.Event{Name: "Krampuskonzert", Time: time.Date(1, 1, 1, 19, 0, 0, 0, time.Local),
+		Uniform:       "Uniform ohne Hut",
+		MusicianPlace: "Gasthaus List",
+		Place:         "Gasthaus List",
+		Date:          time.Date(2018, 12, 5, 0, 0, 0, 0, time.Local),
+		MusicianTime:  time.Date(1, 1, 1, 18, 0, 0, 0, time.Local),
+		Note:          "Anschl. Essen",
+		Important:     true,
+		Internal:      false}
+	saveData(event, true, Josef, t)
+}
+
+func TestFranzInsertOktoberfest(t *testing.T) {
+	event := &model.Event{Name: "Oktoberfest", Time: time.Date(1, 1, 1, 19, 0, 0, 0, time.Local),
+		Uniform:       "Lederhose mit Polo",
+		MusicianPlace: "Gutshof Prosoroff",
+		Place:         "Gutshof Prosoroff",
+		Date:          time.Date(2018, 9, 5, 0, 0, 0, 0, time.Local),
+		MusicianTime:  time.Date(1, 1, 1, 18, 0, 0, 0, time.Local),
+		Note:          "Anschl. Essen",
+		Important:     true,
+		Internal:      false}
+	saveData(event, false, Franz, t)
+}
+
+func TestHelmutInsertWeihnachtsfeier(t *testing.T) {
+	event := &model.Event{Name: "Weihnachtsfeier", Time: time.Date(1, 1, 1, 19, 0, 0, 0, time.Local),
+		Uniform:       "Abendkleidung",
+		MusicianPlace: "Musikheim",
+		Date:          time.Date(2018, 12, 12, 0, 0, 0, 0, time.Local),
+		MusicianTime:  time.Date(1, 1, 1, 18, 0, 0, 0, time.Local),
+		Note:          "Mit Begleitung",
+		Important:     false,
+		Internal:      true}
+	saveData(event, true, Helmut, t)
 }
