0e5f830b54172f363621966824857db7671f2aae
Richard Stöckl <richard.stoeckl@aon.at>
2018-06-14 14:46:30 +0200

    added rand initalizer

--- application/setup.go
+++ application/setup.go
@@ -3,12 +3,15 @@ package application
 import (
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/mvl-at/qbs"
+	"math/rand"
 	"rest/context"
 	"rest/database"
 	"rest/http"
+	"time"
 )
 
 func Setup() {
+	rand.Seed(time.Now().UnixNano())
 	qbs.SetLogger(context.Log, context.ErrLog)
 	qbs.RegisterSqlite3(context.Conf.SQLiteFile)
 	database.Register()
